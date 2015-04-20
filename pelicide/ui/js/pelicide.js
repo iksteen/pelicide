578bbeb1054837632b232b1f7d9322e43d41aa4c
Ingmar Steen <iksteen@gmail.com>
2015-04-20 23:00:16 +0200

    Small cleanup in updatePreview().
    Use empty() instead of .html(‘’).

--- pelicide/ui/js/pelicide.js
+++ pelicide/ui/js/pelicide.js
@@ -346,7 +346,7 @@ define([
                     }
                 })
             } else {
-                jQuery('#preview').html('');
+                jQuery('#preview').empty();
             }
         },
 
