0d16852c88dd22b90d843240c6cbf07eae53745d
Ingmar Steen <iksteen@gmail.com>
2015-04-26 22:04:43 +0200

    Add promise based dialog function to Util.
    The provided form will be augmented with ok() and cancel() methods which
    validate the form and resolve the promise on ok or reject the promise on
    cancel.

--- pelicide/ui/js/util.js
+++ pelicide/ui/js/util.js
@@ -1,7 +1,8 @@
 define([
+    'jquery',
     'unorm',
     'w2ui'
-], function(unorm) {
+], function(jQuery, unorm) {
     var combining = /[\u0300-\u036F]/g;
     var util = {
         getErrorString: function (e) {
@@ -21,6 +22,50 @@ define([
 
         slugify: function (s) {
             return unorm.nfkd(s).replace(combining, '').toLowerCase().replace(/[^\w\s-]/g, '').replace(/[-\s]+/g, '-');
+        },
+
+        dialog: function (config) {
+            var form = config.form;
+
+            return new Promise(function (resolve, reject) {
+                config = jQuery.extend(
+                    {
+                        style: 'padding: 15px 0px 0px 0px',
+                        width: 500,
+                        height: 300
+                    },
+                    config,
+                    {
+                        body: '<div id="popup_form_div" style="width: 100%; height: 100%;"></div>',
+                        onOpen: function (event) {
+                            event.onComplete = function () {
+                                jQuery('#w2ui-popup').find('#popup_form_div').w2render(form);
+                            }
+                        },
+                        onClose: function (e) {
+                            if(!e.options.result) {
+                                reject('cancelled');
+                            } else {
+                                resolve(e.options.result);
+                            }
+                        }
+                    }
+                );
+
+                form.ok = function () {
+                    if (this.validate(true).length == 0) {
+                        w2popup.close({result: jQuery.extend({}, this.record)});
+                        this.clear();
+                    }
+                };
+
+                form.cancel = function () {
+                    w2popup.close({result: null});
+                    this.clear();
+                };
+
+                w2popup.open(config);
+            });
         }
     };
 
