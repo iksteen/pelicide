50db6d867929522c9c2c3de18070b18440dfb672
Ingmar Steen <iksteen@gmail.com>
2015-04-30 23:26:53 +0200

    Convert Pelicide to an ES6 class.

--- pelicide/ui/js/pelicide.js
+++ pelicide/ui/js/pelicide.js
@@ -1,13 +1,14 @@
-define([
-    'js/util',
-    'js/project',
-    'js/editor',
-    'js/preview',
-    'jquery',
-    'vitmalina/w2ui'
-], function(Util, Project, Editor, Preview, jQuery) {
+import Util from 'js/util'
+import Project from 'js/project'
+import Editor from 'js/editor'
+import Preview from 'js/preview'
+import jQuery from 'jquery'
+import 'vitmalina/w2ui'
 
-    function Pelicide(options) {
+export default class Pelicide {
+    constructor(options) {
+        Object.assign(this, w2utils.event);
+
         options = options || {};
 
         this.handlers = [];
@@ -17,92 +18,84 @@ define([
         this.preview = new Preview(this, options);
     }
 
-    Pelicide.prototype = {
-        run: function (box) {
-            var self = this;
+    run(box) {
+        /* Initialise the layout. */
+        this.layout().render(box);
 
-            /* Initialise the layout. */
-            this.layout().render(box);
+        /* Run this as a timeout to allow the DOM to settle. */
+        setTimeout(() => {
+            /* Initialise the project, editor and preview panel. */
+            this.project.render(w2ui['layout'].el('left'), w2ui['layout_left_toolbar']);
+            this.editor.render(w2ui['editor'].el('main'), w2ui['editor_main_toolbar']);
+            this.preview.render(w2ui['editor'].el('right'), w2ui['editor_right_toolbar']);
+        }, 0);
+    }
 
-            /* Run this as a timeout to allow the DOM to settle. */
-            setTimeout(function () {
-                /* Initialise the project, editor and preview panel. */
-                self.project.render(w2ui['layout'].el('left'), w2ui['layout_left_toolbar']);
-                self.editor.render(w2ui['editor'].el('main'), w2ui['editor_main_toolbar']);
-                self.preview.render(w2ui['editor'].el('right'), w2ui['editor_right_toolbar']);
-            }, 0);
-        },
-
-        _ensureToolbarItems: function(layout, items) {
-            if (layout.toolbar === undefined) {
-                layout.toolbar = { items: [] }
-            } else if(layout.toolbar.items === undefined) {
-                layout.toolbar.items = [];
-            }
-            return layout;
-        },
-
-        layout: function (box) {
-            var self = this,
-                projectLayout = this._ensureToolbarItems(this.project.layout() || {}),
-                editorLayout = this._ensureToolbarItems(this.editor.layout() || {}),
-                previewLayout = this._ensureToolbarItems(this.preview.layout() || {});
-
-            editorLayout.toolbar.items = [].concat(
-                [
-                    {
-                        type: 'check',
-                        id: 'project',
-                        icon: 'fa fa-bars',
-                        hint: 'Toggle project view',
-                        checked: true,
-                        onClick: function () { self.toggleProject(); }
-                    },
-                    {type: 'break'}
-                ],
-                editorLayout.toolbar.items,
-                [
-                    {type: 'spacer'},
-                    {
-                        type: 'check',
-                        id: 'preview',
-                        icon: 'fa fa-eye',
-                        hint: 'Toggle preview',
-                        checked: true,
-                        onClick: function () { self.togglePreview(); }
-                    }
-                ]
-            );
-
-            jQuery().w2layout({
-                name: 'layout',
-                panels: [
-                    jQuery.extend({}, projectLayout, { type: 'left', size: 240, resizable: true }),
-                    { type: 'main' }
-                ]
-            });
-
-            jQuery().w2layout({
-                name: 'editor',
-                panels: [
-                    jQuery.extend({}, editorLayout, { type: 'main', size: '50%' }),
-                    jQuery.extend({}, previewLayout, { type: 'right', size: '50%' })
-                ]
-            });
-            w2ui['layout'].content('main', w2ui['editor']);
-
-            return w2ui['layout'];
-        },
-
-        toggleProject: function () {
-            w2ui['layout'].toggle('left');
-        },
-
-        togglePreview: function () {
-            w2ui['editor'].toggle('right');
+    _ensureToolbarItems(layout) {
+        if (layout.toolbar === undefined) {
+            layout.toolbar = { items: [] }
+        } else if(layout.toolbar.items === undefined) {
+            layout.toolbar.items = [];
         }
-    };
-    jQuery.extend(Pelicide.prototype, w2utils.event);
+        return layout;
+    }
 
-    return Pelicide;
-});
+    layout(box) {
+        var projectLayout = this._ensureToolbarItems(this.project.layout() || {}),
+            editorLayout = this._ensureToolbarItems(this.editor.layout() || {}),
+            previewLayout = this._ensureToolbarItems(this.preview.layout() || {});
+
+        editorLayout.toolbar.items = [].concat(
+            [
+                {
+                    type: 'check',
+                    id: 'project',
+                    icon: 'fa fa-bars',
+                    hint: 'Toggle project view',
+                    checked: true,
+                    onClick: () => this.toggleProject()
+                },
+                {type: 'break'}
+            ],
+            editorLayout.toolbar.items,
+            [
+                {type: 'spacer'},
+                {
+                    type: 'check',
+                    id: 'preview',
+                    icon: 'fa fa-eye',
+                    hint: 'Toggle preview',
+                    checked: true,
+                    onClick: () => this.togglePreview()
+                }
+            ]
+        );
+
+        jQuery().w2layout({
+            name: 'layout',
+            panels: [
+                jQuery.extend({}, projectLayout, { type: 'left', size: 240, resizable: true }),
+                { type: 'main' }
+            ]
+        });
+
+        jQuery().w2layout({
+            name: 'editor',
+            panels: [
+                jQuery.extend({}, editorLayout, { type: 'main', size: '50%' }),
+                jQuery.extend({}, previewLayout, { type: 'right', size: '50%' })
+            ]
+        });
+        w2ui['layout'].content('main', w2ui['editor']);
+
+        return w2ui['layout'];
+    }
+
+    toggleProject() {
+        w2ui['layout'].toggle('left');
+    }
+
+    togglePreview() {
+        w2ui['editor'].toggle('right');
+    }
+}
