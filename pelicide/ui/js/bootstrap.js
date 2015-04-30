2502420313624da5955e5b12492d15c4280e0f07
Ingmar Steen <iksteen@gmail.com>
2015-04-30 19:08:16 +0200

    Make main.js more jspm conforming.
    Split up main.js into js/main.js and js/bootstrap.js.

--- pelicide/ui/js/bootstrap.js
+++ pelicide/ui/js/bootstrap.js
@@ -1,17 +1,17 @@
-Promise.all([
-    System.import('jquery'),
-    System.import('js/pelicide'),
-    System.import('js/md-editor'),
-    System.import('js/rst-editor'),
-    System.import('js/article-content'),
-    System.import('js/api'),
-    System.import('js/util')
-]).then(function (m) { (function(jQuery, Pelicide, MDEditor, RSTEditor, ArticleContent, API, Util) {
+import jQuery from 'jquery';
+import API from 'js/api';
+import Pelicide from 'js/pelicide';
+import Util from 'js/util';
+import ArticleContent from 'js/article-content';
+import MDEditor from 'js/md-editor';
+import RSTEditor from 'js/rst-editor';
+
+export function bootstrap() {
     // Set up API endpoint.
     API.configure('/rpc');
 
     // Start Pelicide UI when DOM is ready.
-    jQuery(function() {
+    jQuery(function () {
         API.get('SITENAME').then(function (sitename) {
             document.title = sitename + ' (Pelicide)';
 
@@ -31,4 +31,4 @@ Promise.all([
             //setTimeout(function () { pelicide.editor.open([], 'welcome-to-pelicide.md'); }, 0);
         }).catch(Util.alert);
     });
-}).apply(this, m); });
+}
