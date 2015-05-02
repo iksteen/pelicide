07ca866ab0e9339e213cbbca2e724555dbe33e6e
Ingmar Steen <iksteen@gmail.com>
2015-05-02 13:11:00 +0200

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 
 
-__version__ = '0.0.0dev'
+__version__ = '0.0.0.dev0'
 
 
 requires = [
@@ -21,28 +21,34 @@ def read_file(filename):
 
 setup(
     name='pelicide',
-    packages=['pelicide'],
     version=__version__,
     description='An IDE for Pelican sites.',
     long_description=read_file('README.rst') + '\n' + read_file('changelog.rst'),
-    author='Ingmar Steen',
-    author_email='iksteen@gmail.com',
     url='https://github.com/iksteen/pelicide/',
     download_url='https://github.com/iksteen/pelicide/tarball/v%s' % __version__,
+
+    author='Ingmar Steen',
+    author_email='iksteen@gmail.com',
+    platforms=['Platform Independent'],
+    license='MIT',
+
+    packages=['pelicide'],
     install_requires=requires,
     include_package_data=True,
+    zip_safe=False,
+
     entry_points={
         'console_scripts': [
             'pelicide = pelicide.__main__:main',
         ],
     },
+
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2 :: Only',
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
