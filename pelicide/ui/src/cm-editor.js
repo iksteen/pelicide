d7138a2431a1907520a3b7c527a338eb0462af60
Ingmar Steen <iksteen@gmail.com>
2015-05-03 14:27:27 +0200

    Added basic formatting functions for markdown.

--- pelicide/ui/src/cm-editor.js
+++ pelicide/ui/src/cm-editor.js
@@ -31,6 +31,10 @@ export default class CMEditor {
         this._codeMirror.focus();
     }
 
+    get ctrlOrCmd() {
+      return (CodeMirror.keyMap["default"] == CodeMirror.keyMap.macDefault) ? "Cmd-" : "Ctrl-";
+    }
+
     close() {
         this._codeMirror = null;
     }
@@ -42,4 +46,57 @@ export default class CMEditor {
     get mode() { return 'text/plain'; }
     static get formats() { return []; }
     static get templates() { return {}; }
+
+    insert(text, offset = 0) {
+        var doc = this._codeMirror.getDoc(),
+            cursor = Object.assign({}, doc.getCursor(), {ch: 0});
+
+        doc.replaceSelection(text);
+        doc.setCursor({line: cursor.line, ch: cursor.ch + text.length - offset});
+
+        this._codeMirror.focus();
+    }
+
+    surround(start_decorator, stop_decorator = null, offset = null) {
+        if (stop_decorator === null) {
+            stop_decorator = start_decorator;
+        }
+
+        if(offset === null) {
+            offset = stop_decorator.length;
+        }
+
+        var doc = this._codeMirror.getDoc();
+        doc.replaceSelection(start_decorator + doc.getSelection() + stop_decorator);
+
+        var cursor = doc.getCursor();
+        cursor.ch -= offset;
+        doc.setCursor(cursor);
+
+        this._codeMirror.focus();
+    }
+
+    replaceLinePrefix(search, replace, skipEmpty=true) {
+        var doc = this._codeMirror.getDoc();
+
+        function replacePrefix(lineNo) {
+            var line = doc.getLine(lineNo),
+                match = search.exec(line),
+                startPos = {line: lineNo, ch: 0},
+                endPos = {line: lineNo, ch: (match === null ? 0 : match[0].length)};
+
+            if (skipEmpty && !line)
+                return;
+
+            doc.replaceRange(replace(match), startPos, endPos);
+        }
+
+        var from = doc.getCursor('from').line,
+            to = doc.getCursor('to').line;
+
+        for (let i = from; i <= to; ++i)
+            replacePrefix(i);
+
+        this._codeMirror.focus();
+    }
 }
--- pelicide/ui/src/md-editor.js
+++ pelicide/ui/src/md-editor.js
@@ -2,8 +2,103 @@ import CMEditor from 'src/cm-editor';
 import 'codemirror/mode/markdown/markdown';
 
 export default class MDEditor extends CMEditor {
-    constructor(pelicide, parent_el, content) {
-        super(pelicide, parent_el, content);
+    constructor(editor, parent_el, content) {
+        super(editor, parent_el, content);
+
+        var ctrl = this.ctrlOrCmd;
+
+        editor.addEditorToolbarItems([
+            {
+                id: 'md_bold',
+                icon: 'fa fa-bold',
+                hint: `Bold (${ctrl}B)`,
+                onClick: () => this.bold()
+            },
+            {
+                id: 'md_italic',
+                icon: 'fa fa-italic',
+                hint: `Italic (${ctrl}I)`,
+                onClick: () => this.italic()
+            },
+            {
+                id: 'md_header',
+                icon: 'fa fa-header',
+                hint: `Header (${ctrl}H)`,
+                onClick: () => this.header()
+            },
+            {type: 'break', id: 'md_break_1'},
+            {
+                id: 'md_link',
+                icon: 'fa fa-link',
+                hint: `Insert link (${ctrl}L)`,
+                onClick: () => this.link()
+            },
+            {
+                id: 'md_image',
+                icon: 'fa fa-picture-o',
+                hint: `Insert image (${ctrl}O)`,
+                onClick: () => this.image()
+            },
+            {
+                id: 'md_ul',
+                icon: 'fa fa-list-ul',
+                hint: `Unordered list item (${ctrl}U)`,
+                onClick: () => this.ul()
+            }
+        ]);
+
+        this._codeMirror.setOption("extraKeys", {
+            [ctrl + 'B']: () => this.bold(),
+            [ctrl + 'I']: () => this.italic(),
+            [ctrl + 'H']: () => this.header(),
+            [ctrl + 'L']: () => this.link(),
+            [ctrl + 'O']: () => this.image(),
+            [ctrl + 'U']: () => this.ul()
+        });
+    }
+
+    bold() {
+        this.surround('**');
+    }
+
+    italic() {
+        this.surround('*');
+    }
+
+    header() {
+        this.replaceLinePrefix(/(#+)\s+/, match =>
+            match ? (match[1].length >= 6 ? '' : '#'.repeat(match[1].length + 1) + ' ') : '# '
+        );
+    }
+
+    link() {
+        var doc = this._codeMirror.getDoc();
+        if (doc.somethingSelected()) {
+            let text = doc.getSelection();
+            if (/^\w+:\/\/|^mailto:/.test(text))
+                this.surround('[](', ')', text.length + 3);
+            else
+                this.surround('[', '](http://)', 1);
+        }
+        else
+            this.insert('[](http://)', 10);
+    }
+
+    image() {
+        var doc = this._codeMirror.getDoc();
+        if (doc.somethingSelected()) {
+            let text = doc.getSelection();
+            if (/^\w+:\/\//.test(text))
+                this.surround('![](', ')', text.length + 3);
+            else
+                this.surround('![', ']()', 1);
+        }
+        else
+            this.insert('[]()', 3);
+    }
+
+    ul() {
+        this.replaceLinePrefix(/^\s*[*+-]\s/, match => match ? '': '* ');
     }
 
     get mode() { return 'markdown'; }
