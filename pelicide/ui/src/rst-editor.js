92d9a0f5f559e31563d87ead270d140cf989eede
Ingmar Steen <iksteen@gmail.com>
2015-05-03 15:53:23 +0200

    Add (very basic) formatting for reStructuredText.

--- pelicide/ui/src/rst-editor.js
+++ pelicide/ui/src/rst-editor.js
@@ -2,8 +2,70 @@ import CMEditor from 'src/cm-editor';
 import 'codemirror/mode/rst/rst';
 
 export default class RSTEditor extends CMEditor {
-    constructor(pelicide, parent_el, content) {
-        super(pelicide, parent_el, content);
+    constructor(editor, parent_el, content) {
+        super(editor, parent_el, content);
+
+        var ctrl = this.ctrlOrCmd;
+
+        editor.addEditorToolbarItems([
+            {
+                id: 'md_bold',
+                icon: 'fa fa-bold',
+                hint: `Bold (${ctrl}B)`,
+                onClick: () => this.bold()
+            },
+            {
+                id: 'md_italic',
+                icon: 'fa fa-italic',
+                hint: `Italic (${ctrl}I)`,
+                onClick: () => this.italic()
+            },
+            {type: 'break', id: 'md_break_1'},
+            {
+                id: 'md_link',
+                icon: 'fa fa-link',
+                hint: `Insert link (${ctrl}L)`,
+                onClick: () => this.link()
+            },
+            {
+                id: 'md_ul',
+                icon: 'fa fa-list-ul',
+                hint: `Unordered list item (${ctrl}U)`,
+                onClick: () => this.ul()
+            }
+        ]);
+
+        this._codeMirror.setOption("extraKeys", {
+            [ctrl + 'B']: () => this.bold(),
+            [ctrl + 'I']: () => this.italic(),
+            [ctrl + 'L']: () => this.link(),
+            [ctrl + 'U']: () => this.ul()
+        });
+    }
+
+    bold() {
+        this.surround('**');
+    }
+
+    italic() {
+        this.surround('*');
+    }
+
+    link() {
+        var doc = this._codeMirror.getDoc();
+        if (doc.somethingSelected()) {
+            let text = doc.getSelection();
+            if (/^\w+:\/\/|^mailto:/.test(text))
+                this.surround('` <', '>`_', text.length + 5);
+            else
+                this.surround('`', ' <http://>`_', 3);
+        }
+        else
+            this.insert('` <http://>`_', 12);
+    }
+
+    ul() {
+        this.replaceLinePrefix(/^\s*[*+-]\s/, match => match ? '': '* ');
     }
 
     get mode() { return 'rst'; }
