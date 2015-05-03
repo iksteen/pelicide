ab73ae504fa5ecee07c3e8f2e3ea25d18b96ef41
Ingmar Steen <iksteen@gmail.com>
2015-05-03 23:21:11 +0200

    Extract base class out of ArticleContent.

--- pelicide/ui/src/content/article.js
+++ pelicide/ui/src/content/article.js
@@ -1,72 +1,21 @@
-import {slugify, dialog, alert} from 'src/util'
+import {alert} from 'src/util'
 import API from 'src/api'
-import jQuery from 'jquery'
-import 'phstc/jquery-dateFormat'
-import 'vitmalina/w2ui'
+import BaseContent from 'src/content/base'
 
-function getPathFromRecord(record) {
-    return {
-        path: record.create_in ? record.create_in.split('/') : [],
-        name: slugify(record.title) + '.' + record.extension.id
-    }
-}
-
-export default class ArticleContent {
+export default class ArticleContent extends BaseContent {
     constructor(project) {
-        this.project = project;
+        super(project);
+
         this._draftNodeId = null;
         this._publishedNodeId = null;
-        this._templates = {};
     }
 
     init() {
         this._draftNodeId = this.project.addContentType('Draft articles');
         this._publishedNodeId = this.project.addContentType('Published articles');
 
-        API.list_extensions()
-            .then((pelican_extensions) => {
-                pelican_extensions = new Set(pelican_extensions);
-
-                var extensions = [];
-                for(let e of this.project.pelicide.editor.editors) {
-                    if (e.templates && e.templates.article) {
-                        let template = e.templates.article;
-                        for (let extension of e.extensions.values()) {
-                            if (pelican_extensions.has(extension)) {
-                                this._templates[extension] = template;
-                                extensions.push(extension);
-                            }
-                        }
-                    }
-                }
-
-                var project = this.project;
-                this._form = $().w2form({
-                    name: 'create_article',
-                    style: 'border: 0px; background-color: transparent;',
-                    fields: [
-                        {field: 'title', type: 'text', required: true, html: {caption: 'Title', attr: 'style="width: 250px"'}},
-                        {field: 'category', type: 'combo', html: {caption: 'Category:', attr: 'style="width: 250px"'}},
-                        {field: 'status', type: 'list', required: true, options: {items: ['draft', 'published']}, html: {caption: 'Status', attr: 'style="width: 250px"'}},
-                        {field: 'extension', type: 'list', required: true, options: {items: extensions}, html: {caption: 'File type', attr: 'style="width: 250px"'}},
-                        {field: 'create_in', type: 'combo', html: {caption: 'Create in:', attr: 'style="width: 250px"'}}
-                    ],
-                    record: {},
-                    actions: {
-                        Cancel: function () { this.cancel(); },
-                        Create: function () { this.ok(); }
-                    },
-                    onValidate: function (event) {
-                        var path = getPathFromRecord(this.record);
-                        if (project.getFile(path.path, path.name)) {
-                            event.errors.push({
-                                field: this.get('title'),
-                                error: 'File already exists'
-                            });
-                        }
-                    }
-                });
-
+        return super.init('article')
+            .then(() => {
                 this.project.addCreateContent({
                     text: 'Create article',
                     icon: 'fa fa-newspaper-o',
@@ -85,37 +34,8 @@ export default class ArticleContent {
     }
 
     create() {
-        var categories = this.project.categories;
-
         return API.get('ARTICLE_PATHS')
-            .then(article_paths => {
-                this._form.set('category', {options: {items: categories}});
-                this._form.set('create_in', {options: {items: article_paths}});
-                this._form.record = {
-                    status: 'draft',
-                    extension: this._form.get('extension').options.items[0],
-                    create_in: article_paths[0]
-                };
-
-                return dialog({
-                    title: 'Create article',
-                    form: this._form
-                })
-            })
-            .then(record => {
-                Object.assign(record, {
-                    slug: slugify(record.title),
-                    date: jQuery.format.date(new Date(), 'yyyy-MM-dd HH:mm')
-                });
-
-                var path = getPathFromRecord(record),
-                    body = this._templates[record.extension.id](record);
-
-                return this.project.pelicide.editor.close()
-                    .then(() => API.set_content(path.path, path.name, body))
-                    .then(() => this.project.reload())
-                    .then(() => this.project.pelicide.editor.open(this.project.getFile(path.path, path.name)));
-            })
+            .then(paths => super.create('Create article', paths))
             .catch(alert);
     }
 }
---
+++ pelicide/ui/src/content/base.js
@@ -0,0 +1,103 @@
+import {slugify, dialog} from 'src/util'
+import API from 'src/api'
+import jQuery from 'jquery'
+import 'phstc/jquery-dateFormat'
+import 'vitmalina/w2ui'
+
+function getPathFromRecord(record) {
+    return {
+        path: record.create_in ? record.create_in.split('/') : [],
+        name: slugify(record.title) + '.' + record.extension.id
+    }
+}
+
+export default class BaseContent {
+    constructor(project) {
+        this.project = project;
+        this._form = null;
+        this._templates = {};
+        this._extensions = [];
+    }
+
+    init(type) {
+        return this.loadExtensions(type)
+            .then(() => {
+                this._form = this.form(type);
+            });
+    }
+
+    loadExtensions(type) {
+        return API.list_extensions()
+            .then((pelican_extensions) => {
+                pelican_extensions = new Set(pelican_extensions);
+                for (let e of this.project.pelicide.editor.editors.values()) {
+                    if (e.templates && e.templates[type]) {
+                        let template = e.templates[type];
+                        for (let extension of e.extensions.values()) {
+                            if (pelican_extensions.has(extension)) {
+                                this._templates[extension] = template;
+                                this._extensions.push(extension);
+                            }
+                        }
+                    }
+                }
+            });
+    }
+
+    form(type) {
+        var project = this.project;
+        return $().w2form({
+            name: `create_${type}`,
+            style: 'border: 0px; background-color: transparent;',
+            fields: [
+                {field: 'title', type: 'text', required: true, html: {caption: 'Title', attr: 'style="width: 250px"'}},
+                {field: 'category', type: 'combo', html: {caption: 'Category:', attr: 'style="width: 250px"'}},
+                {field: 'status', type: 'list', required: true, options: {items: ['draft', 'published']}, html: {caption: 'Status', attr: 'style="width: 250px"'}},
+                {field: 'extension', type: 'list', required: true, options: {items: this._extensions}, html: {caption: 'File type', attr: 'style="width: 250px"'}},
+                {field: 'create_in', type: 'combo', html: {caption: 'Create in:', attr: 'style="width: 250px"'}}
+            ],
+            record: {},
+            actions: {
+                Cancel: function () { this.cancel(); },
+                Create: function () { this.ok(); }
+            },
+            onValidate: function (event) {
+                var path = getPathFromRecord(this.record);
+                if (project.getFile(path.path, path.name)) {
+                    event.errors.push({
+                        field: this.get('title'),
+                        error: 'File already exists'
+                    });
+                }
+            }
+        });
+    }
+
+    create(title, paths) {
+        var categories = this.project.categories;
+
+        this._form.set('category', {options: {items: categories}});
+        this._form.set('create_in', {options: {items: paths}});
+        this._form.record = {
+            status: 'draft',
+            extension: this._extensions[0],
+            create_in: paths[0]
+        };
+
+        return dialog({title, form: this._form})
+            .then(record => {
+                Object.assign(record, {
+                    slug: slugify(record.title),
+                    date: jQuery.format.date(new Date(), 'yyyy-MM-dd HH:mm')
+                });
+
+                var path = getPathFromRecord(record),
+                    body = this._templates[record.extension.id](record);
+
+                return this.project.pelicide.editor.close()
+                    .then(() => API.set_content(path.path, path.name, body))
+                    .then(() => this.project.reload())
+                    .then(() => this.project.pelicide.editor.open(this.project.getFile(path.path, path.name)));
+            })
+    }
+}
