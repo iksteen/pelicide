818c7757abaf04eceb00d40ce6ba5d4513838166
Ingmar Steen <iksteen@gmail.com>
2015-05-05 11:28:53 +0200

    Improve demo api.
    You can now create md files and metadata will be parsed / updated.

--- pelicide/ui/src/demo-api.js
+++ pelicide/ui/src/demo-api.js
@@ -1,24 +1,40 @@
 import Showdown from 'showdown';
+import jQuery from 'jquery';
 
-var NA_CALLS = ['restart', 'get_settings', 'set', 'list_extensions', 'build'],
+
+var NA_CALLS = ['restart', 'get_settings', 'set', 'build'],
     CONTENT = [
         {
-            "status": "draft",
-            "name": "welcome-to-pelicide.md",
-            "url": "http://blurringexistence.net",
-            "content": "Title: Welcome to pelicide\nDate: 2015-04-28 08:00\nStatus: draft\nAuthor: Ingmar Steen\nTags: pelicide, demo\n\n## Welcome to the interactive Pelicide demo!\n\n[Pelicide](https://github.com/iksteen/pelicide) is an IDE for sites built with the [Pelican Static Site Generator](http://getpelican.com). This site is an interactive demo of Pelicide's user interface.\n\nBeing a demo, this site is not connected to any back end so some functionality is not available:\n\n- **All changes are volatile.** While you are able to save your changes, none of them will persist after a page reload.\n- **Building HTML files is disabled.** Without building the HTML, the 'Render' preview mode is quite pointless. If you do select the Render preview mode, it shows my website instead.\n- **Limited draft preview.** The renderer used for the draft preview mode in this demo is different from the one in a regular environment. The metadata shows up at the top, there's no code highlighting, etc.",
-            "meta": {
-                "category": "misc",
-                "title": "Welcome to Pelicide",
-                "author": "Ingmar Steen",
-                "reader": "markdown",
-                "date": "2015-04-28 08:00:00",
-                "slug": "welcome-to-pelicide",
-                "tags": ["pelicide", "demo"]
+            name: 'welcome-to-pelicide.md',
+            url: 'http://blurringexistence.net',
+            content: `Title: Welcome to pelicide
+Date: 2015-04-28 08:00
+Status: draft
+Author: Ingmar Steen
+Tags: pelicide, demo
+
+## Welcome to the interactive Pelicide demo!
+
+[Pelicide](https://github.com/iksteen/pelicide) is an IDE for sites built with the [Pelican Static Site Generator](http://getpelican.com). This site is an interactive demo of Pelicide's user interface.
+
+Being a demo, this particular site is not connected to any back end so some functionality is not available:
+
+- **All changes are volatile.** While you are able to save your changes, none of them will persist after a page reload.
+
+- **Building HTML files is disabled.** Without building the HTML, the 'Render' preview mode is quite pointless. If you do select the Render preview mode, it shows my website instead.
+
+- **Limited draft preview.** The renderer used for the draft preview mode in this demo is different from the one in a regular environment.
+
+- **Markdown only.** Only markdown render support is emulated in this demo. reStructuredText is not supported.
+
+Despite these limitations, feel free to explore the user interface! And if you like it, be sure to get [the real thing](https://github.com/iksteen/pelicide)!
+`,
+            meta: {
+                category: 'misc'
             },
-            "type": "pelican.contents.Draft",
-            "mimetype": "text/x-markdown",
-            "dir": []
+            type: 'pelican.contents.Draft',
+            mimetype: 'text/x-markdown',
+            dir: []
         }
     ];
 
@@ -36,25 +52,34 @@ class API {
 
     configure(endpoint) {}
 
+    list_extensions() {
+        return Promise.resolve(['md']);
+    }
+
     get(key) {
         if (key == 'SITENAME') {
             return Promise.resolve('Blurring Existence');
         } else if(key == 'ARTICLE_PATHS') {
             return Promise.resolve([]);
+        } else if(key == 'PAGE_PATHS') {
+            return Promise.resolve(['pages']);
         } else {
             return NotSupported();
         }
     }
 
     render(format, content) {
-        if (format == 'md' || format == 'markdown' || format == 'mdown')
-            return Promise.resolve(this._showdown.makeHtml(content));
+        if (format == 'md' || format == 'markdown' || format == 'mdown' || format == 'mkd') {
+            let pieces = content.split('\n\n');
+            pieces.splice(0, 1);
+            return Promise.resolve(this._showdown.makeHtml(pieces.join('\n\n')));
+        }
         else
             return NotSupported();
     }
 
     list_content() {
-        return Promise.resolve(CONTENT);
+        return Promise.resolve(jQuery.extend(true, [], CONTENT));
     }
 
     get_content(dir, name) {
@@ -69,16 +94,58 @@ class API {
     }
 
     set_content(dir, name, new_content) {
-        dir = dir.join('/');
+        var metadata = new_content.split('\n\n', 1)[0],
+            meta = {};
+
+        for(let piece of metadata.split('\n').values()) {
+            let [key, value] = piece.split(/:\s+/);
+            meta[key.toLowerCase()] = value;
+        }
+
+        var draft = meta.status == 'draft',
+            page = dir && dir[0] == 'pages',
+            type = page ? 'pelican.contents.Page' : draft ? 'pelican.contents.Draft' : 'pelican.contents.Article';
 
         for(let node of CONTENT.values()) {
-            if (node.dir.join('/') == dir && node.name == name) {
-                node.content = new_content;
+            if (node.dir.join('/') == dir.join('/') && node.name == name) {
+                Object.assign(node, {
+                    content: new_content,
+                    meta: {
+                        category: meta.category || 'misc'
+                    },
+                    type
+                });
                 return Promise.resolve();
             }
         }
 
-        return Promise.reject(new Error('Cannot create new files in demo mode.'));
+        CONTENT.push({
+            name: name,
+            url: 'http://blurringexistence.net',
+            content: new_content,
+            meta: {
+                category: meta.category || 'misc'
+            },
+            mimetype: 'text/x-markdown',
+            dir: dir,
+            type
+        });
+
+        return Promise.resolve();
+    }
+
+    delete_content(dir, name) {
+        dir = dir.join('/');
+
+        for(let i = 0; i < CONTENT.length; ++i) {
+            let node = CONTENT[i];
+            if (node.dir.join('/') == dir && node.name == name) {
+                CONTENT.splice(i, 1);
+                return Promise.resolve();
+            }
+        }
+
+        return Promise.reject(new Error('File not found.'));
     }
 }
 
